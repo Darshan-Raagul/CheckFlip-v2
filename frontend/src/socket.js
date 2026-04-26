d9d8a4fcb81072963b6c2a2d19830c749f581de3
Darshan-Raagul <gamesources29@gmail.com>
2026-04-26 19:17:58 +0530

    Updated socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // Connects to your backend server running on port 3001
-const socket = io('http://localhost:3001');
+const socket = io('https://checkflip-v2.onrender.com');
 
 export default socket;
