1d5da4ea93fff625f0f9bb122639d0fa69e5022f
Darshan-Raagul <gamesources29@gmail.com>
2026-04-26 19:19:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://check-flip-v2.vercel.app/"], methods: ["GET", "POST"] }
+  cors: { origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://check-flip-v2.vercel.app"], methods: ["GET", "POST"] }
 });
 
 mongoose.connect(process.env.MONGO_URI)
